2b395b925768838e6bca251f734de699ed106940
xmmn <xiaokai@mininglamp.com>
2020-09-02 10:47:52 +0800

    init

---
+++ layouts/default.vue
@@ -0,0 +1,88 @@
+<template>
+  <el-container>
+    <el-header>
+      <nuxt-link to="/">Nuxt RealWorld</nuxt-link>
+    </el-header>
+    <el-main>
+      <Nuxt />
+    </el-main>
+    <el-footer>Footer</el-footer>
+  </el-container>
+</template>
+
+<style>
+html {
+  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
+    Roboto, 'Helvetica Neue', Arial, sans-serif;
+  font-size: 16px;
+  word-spacing: 1px;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -moz-osx-font-smoothing: grayscale;
+  -webkit-font-smoothing: antialiased;
+  box-sizing: border-box;
+}
+
+*,
+*::before,
+*::after {
+  box-sizing: border-box;
+  margin: 0;
+}
+
+.button--green {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #3b8070;
+  color: #3b8070;
+  text-decoration: none;
+  padding: 10px 30px;
+}
+
+.button--green:hover {
+  color: #fff;
+  background-color: #3b8070;
+}
+
+.button--grey {
+  display: inline-block;
+  border-radius: 4px;
+  border: 1px solid #35495e;
+  color: #35495e;
+  text-decoration: none;
+  padding: 10px 30px;
+  margin-left: 15px;
+}
+
+.button--grey:hover {
+  color: #fff;
+  background-color: #35495e;
+}
+
+a {
+  text-decoration: none;
+}
+
+.el-main {
+  position: absolute;
+  top: 60px;
+  bottom: 60px;
+  overflow: auto;
+}
+
+.el-header {
+  text-align: center;
+  line-height: 60px;
+  position: fixed;
+  top: 0px;
+  width: 100%;
+}
+
+.el-footer {
+  text-align: center;
+  line-height: 60px;
+  position: fixed;
+  bottom: 0px;
+  width: 100%;
+}
+</style>
